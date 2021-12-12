8a1115f63cfc1253002dfcce908d719268d02913
regulate <regulate@gmail.com>
2021-12-12 13:41:23 -0800

    New Params

--- fitsome.sh
+++ fitsome.sh
@@ -1,1 +1,1 @@
-git pull --rebase; python -u fit_arima.py  && git add modelfits ; git commit -a -m 'New Params' ; git pull --rebase && git push
+git pull --rebase; python -u fit_arima.py  && git add modelfits ; git commit -a -m 'New Params' ; git pull --rebase ; git push; 
